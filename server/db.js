65652a86de1a2ed0637f9eadbcc60dc85c45cbdc
Kevin <kevxemail@gmail.com>
2024-05-09 17:58:38 -0400

    API endpoint now writes back to the PostgreSQL database

--- server/db.js
+++ server/db.js
@@ -1,11 +1,8 @@
 const Pool = require("pg").Pool; // Connect to pg database and use Pool to configure
 
 console.log("Initializing database connection pool...");
-const pool = new Pool({
-    // Postgres database connection
-});
+const pool = new Pool({});
 
 module.exports = pool; // Export the pool object to be used, "module" is a self-contained piece of code that can export functionality to other modules.
 
 console.log("Database connection pool initialized");
-console.log(pool);
--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,7 +1,10 @@
 const router = require("express").Router(); // Use Express routing
 const pool = require("../db"); // Take the exported pool object in
+const bcrypt = require("bcrypt"); // Need this to hash passwords
 
 module.exports = router;
+
+// Authentication = Verify user credentials and identity, authorization determines access rights
 
 // registering route
 router.post("/register", async (req, res) => {
@@ -10,15 +13,28 @@ router.post("/register", async (req, res) => {
         const { name, email, password } = req.body; // Same thing as python x1, x2 = list_with_two_items
 
         // 2. Check if user exist (if user exists then throw error)
-        console.log(pool);
         const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
             email,
         ]); // $1 is the email, we want to check if they equal the email
 
-        res.json(user.rows);
+        if (user.rows.length != 0) {
+            // If we get results there are already users with this email
+            return res.status(401).send("User already exists"); //  401 means the person is unauthenticated, 403 is not authorized
+        }
 
         // 3. Bcrypt the user password to hash it
+
+        const saltRound = 10; // Define the complexity of the salt generation
+
+        const bcryptPassword = await bcrypt.hash(password, saltRound); // Await in front of anything that takes time basically
+
         // 4. Enter the new user inside database
+        const newUser = await pool.query(
+            "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
+            [name, email, bcryptPassword] // Return the new user too to confirm
+        );
+
+        res.json(newUser.rows[0]);
         // 5. GEenrating jwt token
     } catch (err) {
         console.error(err.message);
