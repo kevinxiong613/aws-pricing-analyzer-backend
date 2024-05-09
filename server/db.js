471289c178f3914bcb59f99b8ea9434c16a1e407
Kevin <kevxemail@gmail.com>
2024-05-09 18:02:45 -0400

    Reconfigured db.js to use environment variables to avoid exposing  the database details

--- server/db.js
+++ server/db.js
@@ -1,7 +1,15 @@
+require("dotenv").config();
+
 const Pool = require("pg").Pool; // Connect to pg database and use Pool to configure
 
 console.log("Initializing database connection pool...");
-const pool = new Pool({});
+const pool = new Pool({
+    // Postgres database connection
+    user: process.env.PG_USER,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
 
 module.exports = pool; // Export the pool object to be used, "module" is a self-contained piece of code that can export functionality to other modules.
 
