e36b6017bb4fd40a3ddd8d368a3439d672c4ffc6
Kevin <kevxemail@gmail.com>
2024-05-17 19:47:47 -0400

    Minor comment tweaks

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -12,7 +12,7 @@ module.exports = async (req, res, next) => {
         const payload = jwt.verify(jwtToken, process.env.JWT_SECRET); // If it's verified, will return a payload that can be used in routes (it's the user_id)
 
         req.user = payload.user;
-        next(); // Continue with the rest of the route that this was passed into
+        next(); // Continue with the rest of the route that this function was passed in as a parameter
     } catch (err) {
         console.error(err.message + " authorization has failed!");
         return res.status(403).json("Not authorized"); // 403 used for authorization issues
