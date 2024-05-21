04c67ccc0b9cac9e883712fd834332f6150675a4
Kevin <kevxemail@gmail.com>
2024-05-20 23:16:06 -0400

    Implemented gemini API endpoint to support validating a list of ingredients

--- server/routes/gemini.js
+++ server/routes/gemini.js
@@ -5,7 +5,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 router.get("/getRecipes", async (req, res) => {
     try {
         const prompt = req.header("prompt");
-        console.log("bruh" + prompt);
+        console.log(prompt);
         const genAI = new GoogleGenerativeAI(process.env.GEMINI);
         const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
@@ -20,4 +20,21 @@ router.get("/getRecipes", async (req, res) => {
     }
 });
 
+router.get("/verifyIngredients", async (req, res) => {
+    try {
+        const prompt = req.header("prompt");
+        console.log(prompt);
+        const genAI = new GoogleGenerativeAI(process.env.GEMINI);
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        console.log(text);
+        res.json({ text });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Fetching recipes wasn't successful");
+    }
+});
 module.exports = router;
