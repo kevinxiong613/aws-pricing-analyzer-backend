35d7aa3a04e110a5d50fa604c757ac1967d98a63
Kevin <kevxemail@gmail.com>
2024-05-22 22:18:41 -0400

    Updated getRecipe API endpoint to also send back a title of recipe

--- server/routes/gemini.js
+++ server/routes/gemini.js
@@ -33,19 +33,23 @@ router.get("/getRecipe", async (req, res) => {
         console.log(text);
 
         // Manually parse the text into a JSON object
-        const lines = text.split("--"); // Specified in the prompt, make sure to split the lines based off these dashes to seperate ingredients and instructions.
-        const ingredientsLine = lines[0].trim(); // Get the lines and trim out white space
-        const instructionsLine = lines[1].trim();
+        const lines = text.split("--"); // Specified to seperate everything with dashes in the prompt, make sure to split the lines based off these dashes to seperate ingredients and instructions.
+        const titleLine = lines[0].trim(); // Get the lines and trim out white space
+        const ingredientsLine = lines[1].trim();
+        const instructionsLine = lines[2].trim();
+        const title = titleLine.replace("Title: ", "");
         const ingredients = ingredientsLine.replace("Ingredients: ", "").split(", "); // Create lists of the ingredients/instructions to return in a Json
         const instructions = instructionsLine.replace("Instructions: ", "").split(". ");
         instructions[instructions.length - 1] = instructions[
             instructions.length - 1
         ].replace(".", ""); // This will have a period for the last instruction at the end so get rid of it
 
+        console.log(title);
         console.log(ingredients);
         console.log(instructions);
         const jsonObject = {
             // Convert the ingredient/instruction lists into a json
+            title: title,
             ingredients: ingredients,
             instructions: instructions,
         };
